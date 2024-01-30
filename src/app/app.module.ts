91c7c8eccffc3139deb60e92006a79a0a828124b
Amirkhon3223 <amirich3223@outlook.com>
2024-01-30 05:44:22 +0500

    superHeroes1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { HomeComponent } from './pages/home/home.component';
 import { FormsModule } from '@angular/forms';
-import { MatGridListModule } from '@angular/material/grid-list';
 import { HttpClientModule } from '@angular/common/http';
 import { HeroesListComponent } from './components/heroes-list/heroes-list.component';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { HeaderComponent } from './components/header/header.component';
-import { AuthGuard } from './auth/auth-guard.guard';
-import { GuardsCheckEnd, GuardsCheckStart } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -32,13 +24,7 @@ import { GuardsCheckEnd, GuardsCheckStart } from '@angular/router';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-    MatCardModule,
     FormsModule,
-    MatGridListModule,
     HttpClientModule,
   ],
   providers: [],
